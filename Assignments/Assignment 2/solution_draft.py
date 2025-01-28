b421aa3ed205d2fe26da5beadfe11e51f3046842
sophiaBuss0410 <76047079+sophiaBuss0410@users.noreply.github.com>
2025-01-28 19:56:19 +0200

    first commit

---
+++ Assignments/Assignment 2/solution_draft.py
@@ -0,0 +1,109 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+sns.set(style="whitegrid")
+
+def visualize_graphs(graphs, N=100, p=0.1):
+    N = 100
+    id = 1
+    for graph_type in graphs:
+        if graph_type == "fast_gnp_random_graph":
+            G = nx.fast_gnp_random_graph(N, p, directed=True)
+        elif graph_type == "random_k_out_graph":
+            G = nx.random_k_out_graph(N, k=30, alpha=0.9, self_loops=False)
+        elif graph_type == "scale_free_graph":
+            G = nx.scale_free_graph(N)
+        else:
+            continue
+
+        # num_edges = G.number_of_edges()
+        # print(f"{graph_type}: Number of edges = {num_edges}")
+
+        pr = nx.pagerank(G, alpha=0.9)
+        in_degree = dict(G.in_degree())
+        normalized_in_degree = {k: v / N-1 for k, v in in_degree.items()}
+
+        plt.figure(figsize=(8,6))
+        plt.scatter(list(pr.values()), list(normalized_in_degree.values()), c='orange', alpha=0.7)
+        plt.xlabel('PageRank')
+        plt.ylabel('Normalized Degree')
+        plt.title(f'Plot id: {id} PageRank vs. Normalized Degree for {graph_type}')
+        plt.savefig(f'plot-id-{id}-measure-plot-{graph_type}.png')
+        # plt.show()
+
+        plt.figure(figsize=(10,8))
+        pos = nx.spring_layout(G)
+        node_color = [pr[node] for node in G]
+        node_size = [in_degree[node]*30 for node in G]
+        nx.draw_networkx(G, pos, node_color=node_color, node_size=node_size, cmap=plt.cm.coolwarm, with_labels=False,
+                         edge_color='gray', width=0.5, alpha=0.7)
+        plt.title(f'Plot id: {id+1} Network Visualization with PageRank (Color) and Degree (Size) for {graph_type}')
+        plt.savefig(f'plot-id-{id+1}-network-plot-{graph_type}.png')
+        # plt.show()
+
+        id += 2
+
+
+graphs = ["fast_gnp_random_graph", "random_k_out_graph", "scale_free_graph"]
+visualize_graphs(graphs)
+
+
+def visualize_graph(graph, graph_type, id):
+        num_edges = G.number_of_edges()
+        # print(f"{graph_type}: Number of edges = {num_edges}")
+
+        N = len(G.nodes)
+        pr = nx.pagerank(G, alpha=0.9)
+        in_degree = dict(G.in_degree())
+        normalized_in_degree = {k: v / N-1 for k, v in in_degree.items()}
+
+        plt.figure(figsize=(8,6))
+        plt.scatter(list(pr.values()), list(normalized_in_degree.values()), c='orange', alpha=0.7)
+        plt.xlabel('PageRank')
+        plt.ylabel('Normalized Degree')
+        plt.title(f'Plot id: {id} PageRank vs. Normalized Degree for {graph_type}')
+        plt.savefig(f'plot-id-{id}-measure-plot-{graph_type}.png')
+        # plt.show()
+
+        if nx.is_directed(G):
+                G_undirected = G.to_undirected()
+        else:
+                G_undirected = G
+
+        largest_cc = max(nx.connected_components(G_undirected), key=len)
+        G_largest_cc = G_undirected.subgraph(largest_cc).copy()
+        # print(len(G_largest_cc.nodes))
+
+        plt.figure(figsize=(10,8))
+        pos = nx.spring_layout(G_largest_cc)
+        node_color = [pr[node] for node in G_largest_cc]
+        node_size = [in_degree[node]*30 for node in G_largest_cc]
+        nx.draw_networkx(G_largest_cc, pos, node_color=node_color, node_size=node_size, cmap=plt.cm.coolwarm, with_labels=False, 
+                         edge_color='gray', width=0.5, alpha=0.7)
+        plt.title(f'Plot id: {id+1} Largest Component ({len(G_largest_cc.nodes)} nodes) Visualization with PageRank (Color) and Degree (Size) for {graph_type}')
+        plt.savefig(f'plot-id-{id+1}-network-plot-{graph_type}.png')
+        # plt.show()
+
+path = "data/p2p-Gnutella06.txt"
+
+G = nx.read_edgelist(path, nodetype=int, create_using=nx.DiGraph)
+visualize_graph(G, "p2p-Gnutella06", id=7)
+
+###
+path = "data/p2p-Gnutella08.txt"
+
+G = nx.read_edgelist(path, nodetype=int, create_using=nx.DiGraph)
+visualize_graph(G, "p2p-Gnutella08", id=9)
+
+###
+path = "data/p2p-Gnutella09.txt"
+
+G = nx.read_edgelist(path, nodetype=int, create_using=nx.DiGraph)
+visualize_graph(G, "p2p-Gnutella09", id=11)
+
+print("All done!")
---
+++ Assignments/Assignment 3/solution.py
@@ -0,0 +1,134 @@
+# %%
+import warnings
+warnings.filterwarnings('ignore')
+
+import numpy as np
+import seaborn as sns
+import networkx as nx
+import matplotlib.pyplot as plt
+from tqdm.autonotebook import tqdm
+
+import seaborn as sns
+sns.set(style="whitegrid")
+
+def frequencies(N, ps, num_trials):
+    frequencies = []
+
+    for p in ps:
+        connected_count = 0
+        for _ in tqdm(range(num_trials)):
+            G = nx.fast_gnp_random_graph(N, p)
+            if nx.is_connected(G):
+                connected_count += 1
+        
+        frequencies.append(connected_count / num_trials)
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(ps, frequencies, c = 'orange')
+    plt.xlabel('p')
+    plt.ylabel('Empirical Relative Frequency')
+    plt.title('Plot id: 1 Relative Frequency of Graph Being Connected vs p')
+    plt.grid(True)
+    plt.savefig('plot-id-1-empirical-relative-frequency.png')
+    # plt.show()
+
+def size(N, ps, num_trials):
+    avg_largest_cc_size = []
+
+    for p in ps:
+        connected_count = 0
+        largest_cc_sizes = []
+        for _ in tqdm(range(num_trials)):
+            G = nx.fast_gnp_random_graph(N, p)
+            if nx.is_connected(G):
+                connected_count += 1
+            largest_cc = max(nx.connected_components(G), key=len)
+            largest_cc_sizes.append(len(largest_cc))
+        
+        avg_largest_cc_size.append(np.mean(largest_cc_sizes))
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(ps, avg_largest_cc_size, c = 'green')
+    plt.xlabel('p')
+    plt.ylabel('Empirical Average Size of Largest Connected Component')
+    plt.title('Plot id: 2 Average Size of Largest Connected Component vs p')
+    plt.grid(True)
+    plt.savefig('plot-id-2-empirical-average-size.png')
+    # plt.show()
+
+
+# %%
+def frequencies_small(N, ps, num_trials):
+    frequencies = []
+
+    for p in ps:
+        connected_count = 0
+        for _ in tqdm(range(num_trials)):
+            G = nx.fast_gnp_random_graph(N, p)
+            if nx.is_connected(G):
+                connected_count += 1
+        
+        frequencies.append(connected_count / num_trials)
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(ps, frequencies, c = 'orange')
+    plt.xlabel('p')
+    plt.ylabel('Empirical Relative Frequency')
+    plt.title('Plot id: 3 Relative Frequency of Graph Being Connected vs small p')
+    plt.grid(True)
+    plt.savefig('plot-id-3-empirical-relative-frequency-small.png')
+    # plt.show()
+
+def size_small(N, ps, num_trials):
+    avg_largest_cc_size = []
+
+    for p in ps:
+        connected_count = 0
+        largest_cc_sizes = []
+        for _ in tqdm(range(num_trials)):
+            G = nx.fast_gnp_random_graph(N, p)
+            if nx.is_connected(G):
+                connected_count += 1
+            largest_cc = max(nx.connected_components(G), key=len)
+            largest_cc_sizes.append(len(largest_cc))
+        
+        avg_largest_cc_size.append(np.mean(largest_cc_sizes))
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(ps, avg_largest_cc_size, c = 'green')
+    plt.xlabel('p')
+    plt.ylabel('Empirical Average Size of Largest Connected Component')
+    plt.title('Plot id: 4 Average Size of Largest Connected Component vs small p')
+    plt.grid(True)
+    plt.savefig('plot-id-4-empirical-average-size-small.png')
+    # plt.show()
+
+############################################
+
+N = 500
+num_trials = 50
+
+# ps1 = np.linspace(0.001, 0.03, 20)
+# ps2 = np.linspace(0.025, 1, 10)
+# ps = np.concatenate((ps1, ps2))
+
+# frequencies(N, ps, num_trials)
+
+# # %%
+# ps1 = np.linspace(0.001, 0.015, 20)
+# ps2 = np.linspace(0.01, 1, 10)
+# ps = np.concatenate((ps1, ps2))
+
+# size(N, ps, num_trials)
+
+# %%
+ps = np.linspace(0.001, 0.03, 20)
+
+frequencies_small(N, ps, num_trials=100)
+
+# ps = np.linspace(0.001, 0.015, 20)
+
+# size_small(N, ps, num_trials)
+
+
+# %%
